9ee0893799365bc35a6603ad14325169f2502538
Declan ter Veer-Burke <declan@shift72.com>
2025-10-28 15:00:23 +1300

    Fix issues with ios controls within expo-router

--- ios/src/RocketExpoView.swift
+++ ios/src/RocketExpoView.swift
@@ -25,7 +25,6 @@ class RocketExpoView: ExpoView {
         clipsToBounds = true
         playerViewController.player = playerView
         playerViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        addSubview(playerViewController.view)
         
         let delegate = RocketExpoPlayerDelegate(parentViewController: playerViewController, onComplete: {
             self.onPlaybackCompleted([:])
@@ -34,5 +33,29 @@ class RocketExpoView: ExpoView {
             appContext?.jsLogger.fatal("hostname must be set before initialising RocketExpoView")
         }
         self.player = RocketPlayer.init(player: self.playerView, hostname: RocketExpoView.hostname, delegate: delegate)
+        
+        addSubview(playerViewController.view)
+    }
+
+    public override func didMoveToWindow() {
+      // TV is doing a normal view controller present, so we should not execute
+      // this code
+      #if !os(tvOS)
+      playerViewController.beginAppearanceTransition(self.window != nil, animated: true)
+      #endif
+    }
+    
+    public override func willMove(toWindow newWindow: UIWindow?) {
+        if newWindow == nil {
+            DispatchQueue.main.async { [playerView] in
+                playerView.replaceCurrentItem(with: nil)
+            }
+        }
+    }
+    
+    public override func safeAreaInsetsDidChange() {
+      super.safeAreaInsetsDidChange()
+      playerViewController.view.removeFromSuperview()
+      addSubview(playerViewController.view)
     }
 }
